ea385e92f195c04c3073a3fe0ad8c6ddab37b108
sergio ramos <93677896+SergioRD99@users.noreply.github.com>
2024-02-26 13:12:39 -0600

    cambios en el dist
    ajustes en el dist

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/SergioRD99.github.io/',
+export default defineConfig({ 
   plugins: [vue()],
 })
